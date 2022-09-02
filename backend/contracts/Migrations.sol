fedaae2379f8ecbc2077b9db6a8e0758819e7394
Vishal Kumar <57179742+vishal-jpur@users.noreply.github.com>
2022-09-02 23:18:38 +0530

    Update Migrations.sol

--- backend/contracts/Migrations.sol
+++ backend/contracts/Migrations.sol
@@ -1,19 +1,19 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity >=0.4.22 <0.9.0;        //starting solidity
 
-contract Migrations {
+contract Migrations {                     //connecting server
   address public owner = msg.sender;
   uint public last_completed_migration;
 
   modifier restricted() {
     require(
-      msg.sender == owner,
+      msg.sender == owner,                      //owner only mode
       "This function is restricted to the contract's owner"
     );
     _;
   }
 
-  function setCompleted(uint completed) public restricted {
+  function setCompleted(uint completed) public restricted {               //end the results
     last_completed_migration = completed;
   }
 }
